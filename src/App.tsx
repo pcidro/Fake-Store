cbe612322ef804ef55a623f51b6755643b7fc045
Paulo Cidro <paulocidro@gmail.com>
2026-02-26 10:02:28 -0300

    adicionando um carrinho

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
+import Cart from "./assets/cart.svg";
 
 const App = () => {
   interface Rating {
@@ -15,6 +16,7 @@ const App = () => {
     category: string;
     image: string;
     rating: Rating;
+    quantidade?: number;
   }
 
   const [produtos, setProdutos] = useState<iProdutos[] | null>(null);
@@ -22,7 +24,9 @@ const App = () => {
   const [inputProduct, setInputProduct] = useState("");
   const [prices, setPrices] = useState("");
   const [paginaAtual, setPaginaAtual] = useState(1);
-  const itensPorPagina = 10;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [carrinho, setCarrinho] = useState<iProdutos[] | []>([]);
 
   const produtosFiltrados = produtos?.filter((produto) => {
     const matchCategoria =
@@ -45,21 +49,49 @@ const App = () => {
     return 0;
   });
 
+  const itensPorPagina = 10;
   const indiceInicial = (paginaAtual - 1) * itensPorPagina;
   const indiceFinal = paginaAtual * itensPorPagina;
   const itensAtuais = produtosOrdenados.slice(indiceInicial, indiceFinal);
   const totalPaginas = Math.ceil(produtosOrdenados.length / itensPorPagina);
 
+  function addtoCart(produtoClicado: iProdutos) {
+    const inCart = itensAtuais.findIndex(
+      (produto) => produto.id === produtoClicado.id,
+    );
+    if (!inCart) {
+      setCarrinho([...carrinho, { ...produtoClicado, quantidade: 1 }]);
+    } else {
+      const newCarrinho = itensAtuais.map((item) => {
+        if (item.id === produtoClicado.id) {
+          return { ...item, quantidade: (item.quantidade || 0) + 1 };
+        }
+        return item;
+      });
+      setCarrinho(newCarrinho);
+    }
+  }
+
   useEffect(() => {
     async function getProducts() {
-      const res = await fetch("https://fakestoreapi.com/products");
-      const data = await res.json();
-      setProdutos(data);
+      try {
+        setLoading(true);
+        const res = await fetch("https://fakestoreapi.com/products");
+        const data = await res.json();
+        setProdutos(data);
+      } catch (err) {
+        console.log(err);
+        setError("Erro ao buscar produtos");
+      } finally {
+        setLoading(false);
+      }
     }
     getProducts();
   }, []);
 
   if (!produtosFiltrados) return;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <div className="container">
       <aside className="aside">
@@ -144,10 +176,27 @@ const App = () => {
                 - <span className="category">{produto.category}</span>
               </p>
               <p className="description">{produto.description}</p>
-              <img src={produto.image} alt="" />
+              <img className="product-img" src={produto.image} alt="" />
+              <button
+                onClick={() => addtoCart(produto)}
+                className="button-cart"
+              >
+                <img src={Cart} alt="add to cart" />
+              </button>
             </li>
           ))}
         </ul>
+        <div>
+          <h3>Meu carrinho</h3>
+          {carrinho.map((produto) => (
+            <li>
+              <h3>
+                {produto.title} - Quantidade:{produto.quantidade}
+              </h3>
+              <p>Subtotal: {produto.price * (produto.quantidade || 0)}</p>
+            </li>
+          ))}
+        </div>
         <div className="pagination">
           <button
             onClick={() => setPaginaAtual((prev) => prev - 1)}
